8b0675dba13c247d2f7fba133925852ccad3b8f0
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-07-01 16:59:22 -0400

    created psql connection file for prod and dev

---
+++ db/psqlPool.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+let pool; 
+if (process.env.NODE_ENV === 'production') {
+  pool = new Pool({
+    user: process.env.PROD_DB_USR,
+    host: process.env.PROD_HOST,
+    database: process.env.PROD_DB,
+    password: process.env.PROD_DB_PW,
+    ssl: true,
+    port: 5432,
+  });
+} else {
+  pool = new Pool({
+    host: 'localhost',
+    database: process.env.DEV_DB,
+    user: process.env.DEV_DB_USR,
+    password: process.env.DEV_DB_PW,
+  });
+}
+
+pool.connect((err, client, done) => {
+  if (err) return console.error('Unsuccessfully connected to db');
+  console.log('Successfully connected to db!');
+});
+
+module.exports = pool;
