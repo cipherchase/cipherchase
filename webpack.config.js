bdc7007ab3101e31ba97c5ede71300a4ea90a136
David Kim <davidkim024@gmail.com>
2019-07-01 18:37:17 -0400

    added webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'src', 'index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.join(__dirname, '/'),
+    port: 8080,
+    proxy: {
+      '/api': 'http:localhost:3000',
+      '/login': 'http://localhost:3000',
+      '/signup': 'http://localhost:3000'
+    },
+    publicPath: 'http://localhost:8080/build/',
+    hot: false,
+    historyApiFallback: true
+  }
+};
