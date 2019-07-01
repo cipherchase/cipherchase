ab00dac6600c7cf73ce0073da5409ca0689f05c9
David Kim <davidkim024@gmail.com>
2019-07-01 18:40:53 -0400

    added MainContainer.jsx

---
+++ src/containers/MainContainer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class MainContainer extends Component {
+  render() {
+    return <div>This is MainContainer</div>;
+  }
+}
+
+export default MainContainer;
