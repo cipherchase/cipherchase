f46f843443f5063d6349bd70e62c636217ab901b
David Kim <davidkim024@gmail.com>
2019-07-02 14:34:46 -0400

    commit before merge with dev

---
+++ src/containers/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React, { Component, useState } from 'react';
+
+const LandingPage = () => {
+  return <div>From LandingPage</div>;
+};
+
+export default LandingPage;
--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import LandingPage from './LandingPage';
 
-const MainContainer = () => <div>This is MainContainer</div>;
+const MainContainer = () => (
+  <div>
+    This is MainContainer
+    <LandingPage />
+  </div>
+);
 
 export default MainContainer;
