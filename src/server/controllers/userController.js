edbe20fb2dfdd56b90f419e33f223697b57caf2b
Donald Blanc <dxb3662@rit.edu>
2019-07-02 15:02:59 -0400

    added method to get all users in DB

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -2,7 +2,7 @@ const client = require('../models/cipherModel');
 
 function getAllUsers(req, res, next) {
   client.query('SELECT * FROM users', (err, results) => {
-    console.log(results.rows);
+    res.json(results.rows);
   });
 }
 
