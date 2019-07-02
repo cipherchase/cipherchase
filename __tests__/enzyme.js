6bed60060fd5388f0de5b82e4a630346b629751b
Bryan Fong <bryanfong.dev@gmail.com>
2019-07-02 19:52:35 -0400

    changed test folder to tests and moved testing dependencies to dev dependencies

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -1,4 +1,15 @@
-const add = (a, b) => a + b;
-test('adds fn to equal to 5', () => {
-  expect(add(2, 3)).toBe(5);
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Race from '../src/components/Race.jsx';
+import GameContainer from '../src/components/GameContainer.jsx';
+
+configure({ adapter: new Adapter() });
+
+
+describe('React Unit Tests', () => {
+  describe('Race React File', () => {
+
+  })
 });
