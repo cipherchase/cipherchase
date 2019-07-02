d4d408d916d1f7b48af7c2f50b21a53903c1050d
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-07-02 16:38:39 -0400

    added codeContainer.jsx

---
+++ src/containers/CodeContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CodeContainer = () => (
+  <div id="code-container">
+    Lahalahlah
+  </div>
+);
+
+export default CodeContainer;
--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import CodeContainer from './CodeContainer.jsx';
 
 const MainContainer = () => (
   <div>
     <h1>Ciper Chase </h1>
+    <CodeContainer />
   </div>
 );
 
