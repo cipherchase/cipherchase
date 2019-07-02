0188445e00e77e496c2f2033e13a079f2be2a575
Donald Blanc <dxb3662@rit.edu>
2019-07-01 21:29:48 -0400

    edited the Main Container file to use functional components, appended .jsx tag in import

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class MainContainer extends Component {
-  render() {
-    return <div>This is MainContainer</div>;
-  }
-}
+const MainContainer = () => <div>This is MainContainer</div>;
 
 export default MainContainer;
