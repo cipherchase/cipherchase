6fd2a919ce577fd1bb8328159a9776cf911f161d
Donald Blanc <dxb3662@rit.edu>
2019-07-02 14:14:03 -0400

    added table schemas to file

--- src/server/models/cipherSchema.sql
+++ src/server/models/cipherSchema.sql
@@ -1,1 +1,11 @@
-DROP TABLE IF EXISTS cipherchase;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+   id serial primary key, 
+   email text not null unique,
+   firstname varchar,
+   lastname varchar,
+   password varchar,
+   score int,
+   sequence integer
+ );
