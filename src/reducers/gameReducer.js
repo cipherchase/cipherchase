8a80b01fba06043103b300052c2e399b342121e3
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-07-02 19:45:16 -0400

    moved state files back into gameReducer.js

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,5 +1,15 @@
 import * as types from '../actions/actionTypes';
-import initialState from '../state/mockState';
+
+const initialState = {
+  isLoggedIn: false,
+  codeChallenge: `let counter = 0;
+for (let i = 0; i < test.length; i++) {
+  counter += 1;
+}`,
+  cpuSpeed: 50 + Math.random() * 50,
+  score: 0,
+  gameActive: false,
+};
 
 const gameReducer = (state = initialState, action) => {
   switch (action.type) {
--- src/state/initialState.js
+++
@@ -1,6 +0,0 @@
-// To be filled when state variables in store are finalized
-const initialState = {
-
-};
-
-export default initialState;
