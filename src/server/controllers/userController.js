b0343a06c1578592559d696d82152d1f400b9635
Donald Blanc <dxb3662@rit.edu>
2019-07-02 17:12:49 -0400

    added abiity to post

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -1,9 +1,23 @@
 const client = require('../models/cipherModel');
 
-function getAllUsers(req, res, next) {
+const userController = {};
+
+userController.getAllUsers = (req, res, next) => {
   client.query('SELECT * FROM users', (err, results) => {
     res.json(results.rows);
   });
-}
+};
 
-module.exports = { getAllUsers };
+userController.createUser = (req, res, next) => {
+  const {
+    username, password, firstname, lastname, email,
+  } = req.body;
+  console.log(`I signed up ${username}, ${password}, ${firstname}, ${lastname}, ${email}`);
+  client.query('INSERT INTO users (username, password, firstname, lastname, email) VALUES ($1, $2, $3, $4, $5);',
+    [username, password, firstname, lastname, email],
+    (err, results) => {
+
+    });
+};
+
+module.exports = userController;
