46ed4ad69222fd186fd6b5a58fea0ff520fc25ef
Bryan Fong <bryanfong.dev@gmail.com>
2019-07-03 10:10:11 -0400

    revert jest test

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -1,19 +1,4 @@
-import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import toJson from 'enzyme-to-json';
-
-import Race from '../src/components/Race.jsx';
-
-configure({ adapter: new Adapter() });
-
-
-describe('React Unit Tests', () => {
-  let wrapper;
-
-  describe('Race React File', () => {
-    it('test', () => {
-      expect(true.toEqual(true));
-    });
-  })
+const add = (a, b) => a + b;
+test('adds fn to equal to 5', () => {
+  expect(add(2, 3)).toBe(5);
 });
