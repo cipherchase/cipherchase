c722cf2e2bcd1a0d601f3e3c8f59ca63a03eef92
David Kim <davidkim024@gmail.com>
2019-07-03 10:03:01 -0400

    added style

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -2,6 +2,60 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { signup } from '../actions/actionCreators';
 
+// Styling
+const titleStyle = {
+  color: 'black',
+  fontSize: '1.2em',
+  textAlign: 'center',
+};
+
+const box = {
+  margin: 'auto',
+  alignItems: 'center',
+  height: '32em',
+  width: '30em',
+  border: '0.2em solid grey',
+  borderRadius: '8px',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  alignContent: 'center',
+  alignSelf: 'stretch',
+  fontWeight: '900',
+};
+
+const forms = {
+  display: 'flex',
+  alignItems: 'center',
+  borderRadius: '8px',
+  width: '80px',
+  flexDirection: 'column',
+};
+
+const inputStyle = {
+  margin: '7px',
+  width: '15em',
+  display: 'flex',
+  justifyContent: 'center',
+  border: '1px solid grey',
+  backgroundColor: '#f4f4f4',
+  opacity: '0.6',
+  height: '1.7em',
+  borderRadius: '8px',
+};
+
+const submitStyle = {
+  color: 'black',
+  backgroundColor: 'lightblue',
+  border: '1px solid grey',
+  borderRadius: '8px',
+  padding: '0.5em',
+  width: '15em',
+  letterSpacing: '10px',
+  fontFamily: 'serif',
+  margin: '2em',
+};
+
 const mapStateToProps = store => ({
   games: store.games,
 });
@@ -15,7 +69,11 @@ class SignupForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '', password: '', firstname: '', lastname: '', email: '',
+      username: '',
+      password: '',
+      firstname: '',
+      lastname: '',
+      email: '',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -43,49 +101,64 @@ class SignupForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Username:
-          <input
-            name="username"
-            type="text"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            name="password"
-            type="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          First Name:
-          <input
-            name="firstname"
-            type="text"
-            value={this.state.firstname}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Last Name:
-          <input
-            name="lastname"
-            type="text"
-            value={this.state.lastname}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Email:
-          <input name="email" type="email" value={this.state.email} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div style={box}>
+        <h4 style={titleStyle}>
+          <p>Welcome, please sign up</p>
+        </h4>
+        <form style={forms} onSubmit={this.handleSubmit}>
+          <label>
+            Username:
+            <input
+              style={inputStyle}
+              name="username"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              style={inputStyle}
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            First Name:
+            <input
+              style={inputStyle}
+              name="firstname"
+              type="text"
+              value={this.state.firstname}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Last Name:
+            <input
+              style={inputStyle}
+              name="lastname"
+              type="text"
+              value={this.state.lastname}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              style={inputStyle}
+              name="email"
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input style={submitStyle} type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
