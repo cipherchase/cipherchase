766c7165480cb969b1a0a3252f4469e4753ebd37
David Kim <davidkim024@gmail.com>
2019-07-03 11:31:57 -0400

    deleted redux-form

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import gameReducer from './gameReducer';
 
 const reducers = combineReducers({
   games: gameReducer,
-  form: formReducer,
 });
 
 export default reducers;
