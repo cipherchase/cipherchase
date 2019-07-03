8e525dd8419d9d88150e485101b980662b324768
David Kim <davidkim024@gmail.com>
2019-07-03 10:03:42 -0400

    added routes

--- src/containers/LandingPage.jsx
+++ src/containers/LandingPage.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import LoginForm from '../components/LoginForm.jsx';
 import SignupForm from '../components/SignupForm.jsx';
+import Header from './Header.jsx';
 
 const LandingPage = () => (
   <div>
-    <div>
-      LoginForm
-      <LoginForm />
-    </div>
-    <br />
-    <div>
-      SignupForm
-      <SignupForm />
-    </div>
-    <br />
+    <BrowserRouter>
+      <Header />
+      <div>
+        <Route path="/" exact component={LoginForm} />
+        <Route path="/" exact component={SignupForm} />
+      </div>
+    </BrowserRouter>
   </div>
 );
 
