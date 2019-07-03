d84a0ecd66a3587ebe8cf007fe3b233984947b3c
Geoffrey Lin <geoffrey.s.lin@gmail.com>
2019-07-03 13:24:26 -0400

    Redesigned logic in codeContainer.jsx for typing correct letters
    Co-authored-by: Bryan Fong <bryanfong.dev@gmail.com>

--- src/containers/CodeContainer.jsx
+++ src/containers/CodeContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import * as actions from '../actions/actionCreators';
@@ -16,26 +16,40 @@ const mapDispatchToProps = dispatch => (
   }
 );
 
-const Highlight = styled.span`
-  background-color: yellow;
-`;
+const CodeContainer = ({ codeChallenge, charIndex, moveCar }) => {
 
-const CodeContainer = ({ codeChallenge, charIndex, moveCar }) => (
-  <div 
-    style={{ border: '1px solid black', width: '500px', height: '500px' }} 
-    tabIndex="0"
-    onKeyPress={ 
-      e => {
-        if (String.fromCharCode(e.which) === codeChallenge[charIndex]) {
+  useEffect(() => {
+    const correct = document.querySelector('#correct');
+    const incomplete = document.querySelector('#incomplete');
+    correct.innerHTML = codeChallenge.substring(0, charIndex);
+    incomplete.innerHTML = codeChallenge.substring(charIndex);
+  });
+
+  return (
+    <div
+      style={{ border: '1px solid black', width: '500px', height: '500px' }}
+      tabIndex="0"
+      onKeyPress={(e) => {
+        const keyPressCode = String.fromCharCode(e.which);
+        const currentLetter = document.querySelector('#currentLetter');
+        if (keyPressCode === codeChallenge[charIndex]) {
           moveCar();
-        } 
-        
-      } 
+          currentLetter.innerHTML = '';
+        } else {
+          console.log('currentLetter!');
+          currentLetter.innerHTML = codeChallenge.substring(charIndex, charIndex + 1);
+          console.log('ewhich', e.which);
+          console.log('char ', codeChallenge[charIndex]);
+        }
+      }
     }
-  >
-    <Highlight id="highlight">{codeChallenge.substring(0, charIndex)}</Highlight>
-    <span>{codeChallenge.substring(charIndex)}</span>
-  </div>
-);
+    >
+      <span id='correct' style={{ color: 'white', backgroundColor: 'green' }} />
+      <span id='currentLetter' style={{ backgroundColor: 'red' }} />
+      <span id='incomplete' style={{ color: '#222' }} />
+
+    </div>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(CodeContainer);
--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -2,14 +2,12 @@ import * as types from '../actions/actionTypes';
 
 const initialState = {
   isLoggedIn: false,
-  codeChallenge: `let counter = 0;
-for (let i = 0; i < test.length; i++) {
-  counter += 1;
-}`,
+  codeChallenge: 'let counter = 0;<br/>for (let i = 0; i < test.length; i++) {<br/> &nbsp; &nbsp; counter += 1;<br/>}',
   cpuSpeed: 50 + Math.random() * 50,
   score: 0,
   gameActive: false,
   charIndex: 0,
+  isWrongChar: false,
 };
 
 const gameReducer = (state = initialState, action) => {
