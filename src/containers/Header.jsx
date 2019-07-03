c1becef247bd7dac3743719fccaa2634b6db5c23
David Kim <davidkim024@gmail.com>
2019-07-03 10:03:15 -0400

    created Header component

---
+++ src/containers/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div>
+      <h2>
+        <Link to="/">Login</Link>
+      </h2>
+      <h2>
+        <Link to="/">Sign Up</Link>
+      </h2>
+    </div>
+  );
+};
+
+export default Header;
