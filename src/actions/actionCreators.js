c964a8677095ffeec31d30c50f3e0ed4f9bc54ca
David Kim <davidkim024@gmail.com>
2019-07-02 20:15:19 -0400

    added login/signup actions and async auth call

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -8,3 +8,42 @@ import * as types from './actionTypes';
   })
 
 */
+export const login = (username, password) => dispatch => {
+  // replace the url to login route
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: 'davidkim',
+      email: 'davidkim@email.com'
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
+    .then(response => response.json())
+    .then(json => {
+      // simulate user authentication
+      const isAuthenticated = json.email === 'davidkim@email.com';
+      dispatch({ type: types.SIGN_UP, payload: { isLoggedIn: isAuthenticated } });
+    });
+};
+
+export const signup = (username, password) => dispatch => {
+  // replace the fetch url to signup route
+  fetch('https://jsonplaceholder.typicode.com/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      username: 'davidkim',
+      email: 'davidkim@email.com'
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
+    .then(response => response.json())
+    .then(json => {
+      // simulate user authentication
+      const isAuthenticated = json.email === 'davidkim@email.com';
+      dispatch({ type: types.SIGN_UP, payload: { isLoggedIn: isAuthenticated } });
+    });
+};
