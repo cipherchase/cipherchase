bb4dc403d043501fea57023b03a13cfa1cca5ca1
Donald Blanc <dxb3662@rit.edu>
2019-07-03 09:47:18 -0400

    removed the conosle log that exposes risks

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -12,7 +12,6 @@ userController.createUser = (req, res, next) => {
   const {
     username, password, firstname, lastname, email,
   } = req.body;
-  console.log(`I signed up ${username}, ${password}, ${firstname}, ${lastname}, ${email}`);
   client.query('INSERT INTO users (username, password, firstname, lastname, email) VALUES ($1, $2, $3, $4, $5);',
     [username, password, firstname, lastname, email],
     (err, results) => {
