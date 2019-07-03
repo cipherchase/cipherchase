4148feee043978b5be1202b53692d3fbfe8a3abb
David Kim <davidkim024@gmail.com>
2019-07-02 21:29:11 -0400

    eslint fix

--- src/containers/LandingPage.jsx
+++ src/containers/LandingPage.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import LoginForm from '../components/LoginForm';
-import SignupForm from '../components/SignupForm';
+import LoginForm from '../components/LoginForm.jsx';
+import SignupForm from '../components/SignupForm.jsx';
 
 const LandingPage = () => (
+  <div>
     <div>
-      <div>
-        LoginForm
-        <LoginForm />
-      </div>
-      <br />
-      <div>
-        SignupForm
-        <SignupForm />
-      </div>
-      <br />
+      LoginForm
+      <LoginForm />
     </div>
+    <br />
+    <div>
+      SignupForm
+      <SignupForm />
+    </div>
+    <br />
+  </div>
 );
 
 export default LandingPage;
