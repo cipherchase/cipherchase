2ce1bcb5aada374bf3e786d67e7368a6af9b5316
David Kim <davidkim024@gmail.com>
2019-07-03 10:04:03 -0400

    added redux form to reducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import gameReducer from './gameReducer';
 
 const reducers = combineReducers({
   games: gameReducer,
+  form: formReducer,
 });
 
 export default reducers;
