e34b6fbba62251457e9a16b1a8003d75aa1dd586
David Kim <davidkim024@gmail.com>
2019-07-02 21:19:21 -0400

    eslint fix

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -8,42 +8,42 @@ import * as types from './actionTypes';
   })
 
 */
-export const login = (username, password) => dispatch => {
+export const login = (username, password) => (dispatch) => {
   // replace the url to login route
   // console.log(username, password);
   fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: JSON.stringify({
       username: 'davidkim',
-      email: 'davidkim@email.com'
+      email: 'davidkim@email.com',
     }),
     headers: {
-      'Content-type': 'application/json; charset=UTF-8'
-    }
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   })
     .then(response => response.json())
-    .then(json => {
+    .then((json) => {
       // simulate user authentication
       const isAuthenticated = json.email === 'davidkim@email.com';
       dispatch({ type: types.SIGN_UP, payload: { isLoggedIn: isAuthenticated } });
     });
 };
 
-export const signup = (username, password, firstname, lastname, email) => dispatch => {
+export const signup = (username, password, firstname, lastname, email) => (dispatch) => {
   // console.log(username, password, firstname, lastname, email);
   // replace the fetch url to signup route
   fetch('https://jsonplaceholder.typicode.com/users', {
     method: 'POST',
     body: JSON.stringify({
       username: 'davidkim',
-      email: 'davidkim@email.com'
+      email: 'davidkim@email.com',
     }),
     headers: {
-      'Content-type': 'application/json; charset=UTF-8'
-    }
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   })
     .then(response => response.json())
-    .then(json => {
+    .then((json) => {
       // simulate user authentication
       const isAuthenticated = json.email === 'davidkim@email.com';
       dispatch({ type: types.SIGN_UP, payload: { isLoggedIn: isAuthenticated } });
