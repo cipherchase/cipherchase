4c4eb44687ee62b3ed972a5d0fe798dcc44977e8
Bryan Fong <bryanfong.dev@gmail.com>
2019-07-04 17:59:35 -0400

    logic works to make car move linearly.

--- src/containers/CodeContainer.jsx
+++ src/containers/CodeContainer.jsx
@@ -23,10 +23,17 @@ const CodeContainer = ({
     const currentLetter = document.querySelector('#currentLetter');
     currentLetter.style.backgroundColor = 'yellow';
 
-    if (codeChallenge.substring(charIndex, charIndex + 5) === '<br/>' && e.which === 13) movePlayer(5); // Enter
-    else if (codeChallenge.substring(charIndex, charIndex + 6) === '&nbsp;' && e.which === 32) movePlayer(6); // Space
-    else if (keyPressChar === codeChallenge[charIndex]) movePlayer(1); // Correct char
-    else currentLetter.style.backgroundColor = 'red';
+    if (codeChallenge.substring(charIndex, charIndex + 5) === '<br/>' && e.which === 13) {
+      movePlayer(5); // Enter
+    } else if (codeChallenge.substring(charIndex, charIndex + 6) === '&nbsp;') {
+      if (codeChallenge.substring(charIndex, charIndex + 12) !== '&nbsp;&nbsp;' && e.which === 32) {
+        movePlayer(6); // Space
+      }
+    } else if (keyPressChar === codeChallenge[charIndex]) {
+      movePlayer(1); // Correct char
+    } else {
+      currentLetter.style.backgroundColor = 'red';
+    }
   };
 
   const handleTabPress = (e) => {
