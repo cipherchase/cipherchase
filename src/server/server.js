2a68e8ebfc9f32398e1ead3be4006046c93abaa6
Donald Blanc <dxb3662@rit.edu>
2019-07-06 17:28:58 -0400

    fix npm start

--- src/server/server.js
+++ src/server/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../../dist/')));
+app.use(express.static(path.join(__dirname, '../../')));
 app.use((req, res, next) => {
   console.log(req.body);
   next();
