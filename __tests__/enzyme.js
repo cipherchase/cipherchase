0ee3474b357f25e55451d6ce9fb5f4aebc2464eb
Bryan Fong <bryanfong.dev@gmail.com>
2019-07-06 21:31:56 -0400

    added babel-plugin-transform-es2015-modules-commonjs

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -1,4 +1,10 @@
-const add = (a, b) => a + b;
-test('adds fn to equal to 5', () => {
-  expect(add(2, 3)).toBe(5);
-});
+import gameReducer from '../src/reducers/gameReducer';
+
+
+describe('Test', () => {
+  describe('Test', () => {
+    it('test again', () => {
+      expect('bryan').toEqual('bryan');
+    })
+  })
+})
